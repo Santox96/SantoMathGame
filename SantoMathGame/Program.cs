2cefb994dcaf572c129dd33100f6386978a9ae60
USER <USER@DESKTOP-EATI25B>
2023-07-23 12:11:49 -0400

    Completed!!!#

--- SantoMathGame/Program.cs
+++ SantoMathGame/Program.cs
@@ -1,83 +1,15 @@
-﻿var date = DateTime.UtcNow;
-string name = GetName();
+﻿using SantoMathGame;
+using static System.Formats.Asn1.AsnWriter;
 
-Menu(name);
-string GetName()
-{
-    Console.WriteLine("Please type your name");
+var menu = new Menu(); 
 
-    var name = Console.ReadLine();
-    return name;
-}
-void Menu(string name)
-{
-    Console.WriteLine("-----------------------------------------");
-    Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. this is your math's game.\n");
-    Console.WriteLine(@$"Choose a game for today!!!
-A - Addition
-S - Subtraction
-M - Multiplication 
-D - Division
-Q - Quit the game");
-    Console.WriteLine("-----------------------------------------");
+var date = DateTime.UtcNow;
 
-    var gameSelected = Console.ReadLine();
+//var games = new List<string>();
 
-    switch (gameSelected.Trim().ToLower())
-    {
-        case "a":
-            AdditionGame("Addition game selected");
-            break;
-        case "s":
-            SubstractionGame("Substraction game selected");
-            break;
-        case "m":
-            MultiplicationGame("Multiplication game selected");
-            break;
-        case "d":
-            DivisionGame("Division game selected");
-            break;
-        case "q":
-            Console.WriteLine("Good bye!!!!!");
-            Environment.Exit(1);
-            break;
-        default:
-            Console.WriteLine("Invalid input :v ");
-            Environment.Exit(1);
-            break;
-    }
-}
-void AdditionGame(string message)
-{
-    Console.WriteLine(message);
+string name = Helpers.GetName();
 
-    var random = new Random();
-    int firstNumber = random.Next(1, 9);
-    int secondNumber = random.Next(1, 9);
+menu.ShowMenu(name, date);
 
-    Console.WriteLine($"{firstNumber} + {secondNumber}");
-    var result = Console.ReadLine();
 
-    if (int.Parse(result) == firstNumber + secondNumber)
-    {
-        Console.WriteLine("Your answer was correct!");
-    }
-    else
-    {
-        Console.WriteLine("Your answer was incorrect!");
-    }
-
-}
-void SubstractionGame(string message)
-{
-    Console.WriteLine();
-}
-void MultiplicationGame(string message)
-{
-    Console.WriteLine(message);
-}
-void DivisionGame(string message)
-{
-    Console.WriteLine(message);
-}
 
